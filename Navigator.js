a318781ab0e10ac2c80f3b2173a742fa9d886ebb
DunniAdenuga <ia005@bucknell.edu>
2017-11-28 19:24:00 -0500

    added login and ingredientsList

--- Navigator.js
+++ Navigator.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StackNavigator } from 'react-navigation'
 import Recipes from './screens/Recipes'
+import LoginScreen from './screens/Login'
+import IngredientsListScreen from './screens/IngredientsList'
 import { View, Button, Text } from 'react-native'
 
 const RecipeScreen = () => (
@@ -18,6 +20,8 @@ const DetailsScreen = ({ navigation }) => (
 );
 
 const AppNavigator = StackNavigator({
+  First: { screen: LoginScreen },
+  Second: { screen: IngredientsListScreen },
   Details: {
     screen: DetailsScreen,
     navigationOptions: {
